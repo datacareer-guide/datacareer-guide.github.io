2778e3d5b757b85269ec3c5123c374316ef84bf5
1cadumagalhaes <magalhaescarlosedu@gmail.com>
2022-10-08 19:44:29 -0300

    feat: adding consent mode

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -7,3 +7,42 @@ var $ = function (...args) {
 document.addEventListener("DOMContentLoaded", function (event) {
   console.log('hellooo')
 });
+
+// obtain plugin
+var cc = initCookieConsent();
+
+import pt from '../data/consent.json' assert { type: 'json' };
+
+import en from '../../en/_assets/data/consent.json' assert { type: 'json' };
+
+// run plugin with your configuration
+cc.run({
+  current_lang: 'pt',
+  autoclear_cookies: true,                   // default: false
+  page_scripts: true,                        // default: false
+  gui_options: {
+    consent_modal: {
+      layout: 'cloud',
+    },
+    settings_modal: {
+      layout: 'box'
+    }
+  },
+
+  onFirstAction: function (user_preferences, cookie) {
+    // callback triggered only once
+  },
+
+  onAccept: function (cookie) {
+    // ...
+  },
+
+  onChange: function (cookie, changed_preferences) {
+    // ...
+  },
+
+  languages: {
+    'pt': pt,
+    'en': en
+  }
+});
